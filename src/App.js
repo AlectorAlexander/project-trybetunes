652aed9a8151ea0d733ae0db9213d86c073a0508
Aléctor Alexander <alector.alexandre@gmail.com>
2022-03-07 15:15:07 -0300

    Criando as rotas necessarias (Requisito 1)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Album from './pages/album';
+import Favorites from './pages/favorites';
+import Login from './pages/login';
+import Profile from './pages/profile';
+import ProfileEdit from './pages/profileEdit';
+import Seach from './pages/search';
+import NotFound from './pages/NotFound';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes</p>);
+    return (
+      <div>
+        <Switch>
+          <Route path="/" exact component={ Login } />
+          <Route path="/search" component={ Seach } />
+          <Route path="/album/:id" component={ Album } />
+          <Route path="/favorites" component={ Favorites } />
+          <Route path="/profile" exact component={ Profile } />
+          <Route path="/profile/edit" exact component={ ProfileEdit } />
+          <Route component={ NotFound } />
+        </Switch>
+      </div>
+    );
   }
 }
 
---
+++ src/pages/search.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Seach extends Component {
+  render() {
+    return (
+      <div data-testid="page-search">
+        <p>Seach</p>
+      </div>
+    );
+  }
+}
+
+export default Seach;

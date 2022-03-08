768d2df99319a06eb08231ec91f31ea27b816b87
Aléctor Alexander <alector.alexandre@gmail.com>
2022-03-08 14:43:39 -0300

    Criando formulario para identificaçao

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Album from './pages/album';
 import Favorites from './pages/favorites';
 import Login from './pages/login';
@@ -7,13 +7,50 @@ import Profile from './pages/profile';
 import ProfileEdit from './pages/profileEdit';
 import Seach from './pages/search';
 import NotFound from './pages/NotFound';
+import Loading from './pages/loading';
+import { createUser, getUser } from './services/userAPI';
 
 class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      loading: false,
+      logado: false,
+      name: '',
+    };
+  }
+
+  saveUser = () => {
+    const { name } = this.state;
+    createUser({ name });
+    this.setState({ loading: true }, this.validateUser);
+  }
+
+  validateUser = async () => {
+    const { name } = await getUser();
+    if (name !== '') { this.setState({ loading: false, logado: true }); }
+  }
+
+  textLength = ({ target }) => {
+    const { value } = target;
+    const { length } = value;
+    if (length >= (2 + 1)) { this.setState({ name: value }); }
+  }
+
   render() {
+    const { loading, name, logado } = this.state;
     return (
       <div>
         <Switch>
-          <Route path="/" exact component={ Login } />
+          {loading && <Route path="/" component={ Loading } />}
+          <Route path="/" exact>
+            {logado ? <Redirect to="/search" /> : <Login
+              onChange={ this.textLength }
+              disabled={ name.length < (2 + 1) }
+              onClick={ this.saveUser }
+            />}
+          </Route>
           <Route path="/search" component={ Seach } />
           <Route path="/album/:id" component={ Album } />
           <Route path="/favorites" component={ Favorites } />
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends Component {
   render() {
+    const { onClick, disabled, onChange } = this.props;
     return (
       <div data-testid="page-login">
         <p>Login</p>
+        <label htmlFor="name">
+          <span>
+            Nome:
+            {' '}
+          </span>
+          <input
+            data-testid="login-name-input"
+            onChange={ onChange }
+            type="text"
+            name="name"
+          />
+        </label>
+        <button
+          disabled={ disabled }
+          onClick={ onClick }
+          data-testid="login-submit-button"
+          type="button"
+        >
+          Entrar
+        </button>
       </div>
     );
   }
 }
+
+Login.propTypes = {
+  onClick: PropTypes.bool.isRequired,
+  onChange: PropTypes.func.isRequired,
+  disabled: PropTypes.bool.isRequired,
+};
 
 export default Login;

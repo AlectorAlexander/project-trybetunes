991b6b4b576262a0549aa4a14f166552f3d415d5
Aléctor Alexander <alector.alexandre@gmail.com>
2022-03-09 13:05:24 -0300

    adicionando links no Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from '../pages/loading';
 import { getUser } from '../services/userAPI';
 
@@ -24,6 +25,9 @@ render() {
   return (
     <header data-testid="header-component">
       <span data-testid="header-user-name">{name === '' ? <Loading /> : name}</span>
+      <Link data-testid="link-to-search" to="/search"> Search </Link>
+      <Link data-testid="link-to-favorites" to="/favorites"> Favoritas </Link>
+      <Link data-testid="link-to-profile" to="/profile"> Perfil </Link>
     </header>
   );
 }

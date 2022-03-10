321974d77cc03229fdec1fb95b8aeffaa092c2dd
Aléctor Alexander <alector.alexandre@gmail.com>
2022-03-09 22:37:23 -0300

    adicionando campo de pesquisa de albuns

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,40 +1,88 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Loading from './loading';
 
 class Seach extends Component {
   constructor() {
     super();
     this.state = {
       search: 0,
+      value: '',
+      loading: false,
+      searchClick: false,
+      nameArtist: '',
+      artistsObject: [],
     };
+  }
+
+  searchBand = async () => {
+    this.setState({ searchClick: true, loading: true });
+    const { value } = this.state;
+    const artists = await searchAlbumsAPI(value);
+    this.setState({ loading: false, search: 0, value: '', artistsObject: artists });
+    // this.setState((estadoAnterior) => ({
+    //   artistsObject: [estadoAnterior, artists],
+    // }), console.log(artists));
   }
 
   textLength = ({ target }) => {
     const { value } = target;
     const { length } = value;
-    if (length >= 2) { this.setState({ search: value }); }
+    this.setState({ search: length, value, nameArtist: value });
   }
 
   render() {
-    const { search } = this.state;
+    const { value, search, loading, searchClick, nameArtist, artistsObject } = this.state;
     return (
       <div data-testid="page-search">
-        <label htmlFor="search">
-          <input
-            data-testid="search-artist-input"
-            placeholder="Nome do Artista"
-            onChange={ this.textLength }
-            type="text"
-            name="search"
-          />
-        </label>
-        <button
-          disabled={ search < 2 }
-          onClick={ console.log('bla') }
-          data-testid="search-artist-button"
-          type="button"
-        >
-          Pesquisar
-        </button>
+        {loading ? <Loading />
+          : (
+            <>
+              <label htmlFor="search">
+                <input
+                  data-testid="search-artist-input"
+                  value={ value }
+                  placeholder="Nome do Artista"
+                  onChange={ this.textLength }
+                  type="text"
+                  name="search"
+                />
+              </label>
+              <button
+                disabled={ search < 2 }
+                onClick={ this.searchBand }
+                data-testid="search-artist-button"
+                type="button"
+              >
+                Pesquisar
+              </button>
+            </>)}
+        {searchClick && artistsObject.length > 0
+          ? (
+            <div className="searchArtists">
+              <p>
+                {`Resultado de álbuns de: ${nameArtist}`}
+              </p>
+              <div className="artistsContainer">
+                {artistsObject.map((band) => (
+                  <>
+                    <img
+                      src={ band.artworkUrl100 }
+                      alt={ band.collectionName }
+                      key={ band.collectionId }
+                    />
+                    <Link
+                      data-testid={ `link-to-album-${band.collectionId}` }
+                      to={ `/album/${band.collectionId}` }
+                    >
+                      <h4>{band.artistName}</h4>
+                    </Link>
+                    <h5>{band.collectionName}</h5>
+                  </>
+                ))}
+              </div>
+            </div>) : artistsObject.length === 0 && (<p>Nenhum álbum foi encontrado</p>) }
       </div>
     );
   }
